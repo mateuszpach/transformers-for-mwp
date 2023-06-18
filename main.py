27bc02d8e7afce717ec948805499fb2c93a11ba7
wronkam <michal.wronka00@gmail.com>
2023-06-18 22:14:04 +0200

    interactive prompting strategy as finite automata and some utility

--- main.py
+++ main.py
@@ -4,12 +4,25 @@ import csv
 import time
 import json
 import argparse
+import re
+
+import utils
 import wandb
 
 from utils import extract_number_from_answer
 
 # OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+key = os.getenv("OPENAI_API_KEY")
+if key is None:
+    try:
+        with open("OPENAI_API_KEY") as f:
+            for line in f:
+                key = line.strip()
+                break
+    except FileNotFoundError:
+        print("Neither OPENAI_API_KEY file nor environmental variable was found ")
+        exit(1)
+openai.api_key = key
 
 run = wandb.init(
     # Set the wandb project where this run will be logged
@@ -47,26 +60,93 @@ def get_response(prompt_input, eng, max_tokens, temperature):
     """
     Send request using OpenAI's Chat API.
     """
-    response = openai.ChatCompletion.create(
-        model=eng,
-        messages=prompt_input,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
+    while True:
+        try:
+            response = openai.ChatCompletion.create(
+                model=eng,
+                messages=prompt_input,
+                temperature=temperature,
+                max_tokens=max_tokens,
+            )
+            time.sleep(3)
+            return response
+        except openai.error.RateLimitError as e:
+            print('.', end='', flush=True)
+            time.sleep(2)
 
     return response
 
 
-def get_answer_from_model(prompt, question, eng, max_tokens, temperature):
+def interactive_get_answer_from_model(prompts, states, problem, question, assistant_role, args):
     """
     Get an answer from model.
     """
-    if eng == "gpt-3.5-turbo":
+    eng = args.eng
+    max_tokens = args.max_tokens
+    temperature = args.temp
+    if "gpt-3.5-turbo" in eng:
+        initial_prompt = prompts['START']
+        current_state = 'START'
+        initial_prompt = initial_prompt.replace(args.interactive_strategy_Q_tag, question)
+        initial_prompt = initial_prompt.replace(args.interactive_strategy_P_tag, problem)
+        conversation = [
+            {"role": "system", "content": assistant_role},
+        ]
+        new_prompt = initial_prompt
+        while max_tokens > 0 and current_state != 'END':
+            conversation.append({"role": "user", "content": new_prompt})
+
+            # print("Q",new_prompt)
+            response = get_response(conversation, eng, max_tokens, temperature)
+            # print("A",response['choices'][0]['message']["content"].strip())
+
+            max_tokens -= response['usage']['total_tokens']
+            response_msg = response['choices'][0]['message']
+            response_content = response['choices'][0]['message']['content'].strip()
+            conversation.append(response_msg)
+            for regex, state in states[current_state].items():
+                # Normally the dot matches any character except newlines.
+                if re.fullmatch(regex, response_content.lower(), re.DOTALL) is not None:
+                    current_state = state
+                    break
+            if current_state == 'END':
+                # there is no prompts['END']
+                break
+            new_prompt = prompts[current_state]
+            new_prompt = new_prompt.replace(args.interactive_strategy_Q_tag, question)
+            new_prompt = new_prompt.replace(args.interactive_strategy_P_tag, problem)
+
+        if current_state != 'END' and args.interactive_strategy_failsafe is not None:
+            new_prompt = prompts[args.interactive_strategy_failsafe]
+            new_prompt = new_prompt.replace(args.interactive_strategy_Q_tag, question)
+            new_prompt = new_prompt.replace(args.interactive_strategy_P_tag, problem)
+            conversation.append({"role": "user", "content": new_prompt})
+
+            # print("Q",new_prompt)
+            response = get_response(conversation, eng, args.max_tokens // 2, temperature)
+            # print("A",response['choices'][0]['message']["content"].strip())
+
+            max_tokens -= response['usage']['total_tokens']
+            response_msg = response['choices'][0]['message']
+            conversation.append(response_msg)
+        # print(f"Used {args.max_tokens - max_tokens} tokens")
+        return response['choices'][0]['message']["content"].strip(), conversation
+
+    else:
+        raise ValueError(f"Engine {eng} not supported!")
+
+
+def get_answer_from_model(prompt, question, assistant_role, eng, max_tokens, temperature):
+    """
+    Get an answer from model.
+    """
+    if "gpt-3.5-turbo" in eng:
         prompt_input = prompt + "\n\nQ: {}\nA:".format(question)
-        response = get_response([
-            {"role": "system", "content": "You are an assistant who helps with math problems."},
+        conversation = [
+            {"role": "system", "content": assistant_role},
             {"role": "user", "content": prompt_input},
-        ], eng, max_tokens, temperature)
+        ]
+        response = get_response(conversation, eng, max_tokens, temperature)
 
         return response['choices'][0]['message']["content"].strip()
 
@@ -78,12 +158,32 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", default="gsm8k", type=str, help="Dataset to use")
     parser.add_argument("--eng", default="gpt-3.5-turbo", type=str, help="Engine")
+    # gpt-3.5-turbo-0613 has 60 responses per minute limit (default is 3)
     parser.add_argument("--temp", default=0.0, type=float, help="Temperature for generation")
-    parser.add_argument("--max_tokens", default=1024, type=int, help="Max # of tokens for generation")
+    parser.add_argument("--max_tokens", default=1024, type=int, help="Max # of tokens for generation. In interactive "
+                                                                     "mode, at most 1.5*max_tokens will be used "
+                                                                     "through out whole conversation")
     parser.add_argument("--test_size", default=3, type=int, help="Size of the dataset to test")
     parser.add_argument("--prompt", default="gsm8k", type=str, help="Prompt to use")
+    parser.add_argument("--assistant_role", default="default", type=str,
+                        help="Assistants role in the reasoning process. Available: default/concise")
+    parser.add_argument("--split_PQ", default=False, type=bool,
+                        help="Split task into Problem and Question parts (used only in interactive strategy)")
+    parser.add_argument("--interactive_strategy", default=False, type=bool,
+                        help="If graph state interactive strategy is provided")
+    parser.add_argument("--interactive_strategy_Q_tag", default="##Q##", type=str,
+                        help="Tag used in int. strategy that is to be replaced with question")
+    parser.add_argument("--interactive_strategy_P_tag", default="##P##", type=str,
+                        help="Tag used in int. strategy that is to be replaced with question")
+    parser.add_argument("--interactive_strategy_failsafe", default=None, type=str,
+                        help="State from which to prompt as a last chance to obtain the answer if max_tokens was reached without producing the answer")
 
     args = parser.parse_args()
+    assistants = {
+        'default': 'You are an assistant who helps with math problems.',
+        'concise': 'You are a concise mathematician who carefully solves maths problems',
+    }
+    assistant = assistants[args.assistant_role]
 
     config.dataset = args.dataset
     config.eng = args.eng
@@ -104,44 +204,62 @@ def main():
         for number in answers:
             writer.writerow([number])
 
-    # Load the initial prompt
-    with open(f"prompt/{args.dataset}/{args.prompt}.txt", "r", encoding='utf-8') as f:
-        prompt = f.read().strip()
+    prompts = None
+    states = None
+    prompt = None
+
+    if args.interactive_strategy:
+        # Load interactive strategy prompts and state graph
+        with open(f"prompt/{args.dataset}/{args.prompt}.txt", "r", encoding='utf-8') as f:
+            prompts, states = utils.load_strategy(f)
+        model_output_logs = open(f'dataset/gsm8k/model_output_{args.prompt}_logs.txt', 'w')
+    else:
+        # Load the initial prompt
+        with open(f"prompt/{args.dataset}/{args.prompt}.txt", "r", encoding='utf-8') as f:
+            prompt = f.read().strip()
 
     config.prompt = prompt
 
     count = 0
     correct = 0.0
+    acc = 0
     model_output = open(f'dataset/gsm8k/model_output_{args.prompt}.txt', 'w')
 
     table = wandb.Table(columns=["question", "answer"])
-    for (question, answer) in qa_pairs[:args.test_size]:
+
+    for num, (question, answer) in enumerate(qa_pairs[:min(args.test_size, len(qa_pairs))]):
         count += 1
-        while True:
-            try:
-                # Try to get the answer
-                model_answer = get_answer_from_model(prompt, question,
-                                                     eng=args.eng, max_tokens=args.max_tokens,
-                                                     temperature=args.temp)
+        problem = ''
+        if args.split_PQ:
+            problem, question = utils.split_question(question)
+        if args.interactive_strategy:
+            model_answer, conversation = interactive_get_answer_from_model(prompts=prompts, states=states,
+                                                                           problem=problem, question=question,
+                                                                           assistant_role=assistant, args=args)
+            model_output_logs.write(f'P:"{problem}"\nQ:"{question}"\nConv:{utils.log_conversation(conversation)}\n')
+        else:
+            # Try to get the answer
+            model_answer = get_answer_from_model(prompt=prompt, question=question, assistant_role=assistant,
+                                                 eng=args.eng, max_tokens=args.max_tokens, temperature=args.temp)
 
-                model_answer = ' '.join(model_answer.split())
-                table.add_data(question, model_answer)
+        model_answer = ' '.join(model_answer.split())
+        table.add_data(question, model_answer)
 
-                model_output.write(model_answer + '\n')
+        model_answer_value = extract_number_from_answer(model_answer)
 
-                model_answer = extract_number_from_answer(model_answer)
+        model_output.write(f"The correct answer is: #{answer}#\n")
+        model_output.write(f'Model answer: #{model_answer_value}# extracted from "{model_answer}"\n')
+        model_output.write('-' * 50 + '\n')
+        if args.interactive_strategy:
+            model_output_logs.write(f"The correct answer is: #{answer}#\n")
+            model_output_logs.write(f'Model answer: #{model_answer_value}# extracted from "{model_answer}"\n')
+            model_output_logs.write('-' * 50 + '\n')
 
-                if model_answer == answer:
-                    correct += 1
+        if model_answer_value == extract_number_from_answer(answer):
+            correct += 1
 
-                acc = correct / count
-                print("Correct ratio: ", acc)
-                time.sleep(3)
-                break
-
-            except Exception as e:
-                print(repr(e))
-                time.sleep(5)
+        acc = correct / count
+        print("Correct ratio: ", acc)
 
     model_output.close()
     run.log({"accuracy": acc})
--- utils.py
+++ utils.py
@@ -4,6 +4,26 @@ import re
 def extract_number_from_answer(answer):
     """
     Extract the numerical value from model's answer.
+    Finds the last number in the answer
+    """
+    j = 0
+    answer = answer.replace(',', '')  # changes 50,000 into 50000
+    n = len(answer)
+    ans = -1
+    answer = answer + '.'
+    for i in range(n):
+        if j <= i:
+            j = i + 1
+        if answer[i] == '-':  # float('-') would throw ValueError despite being correct number prefix
+            j = max(j, i + 2)
+        while j <= n:
+            try:
+                tmp_ans = float(answer[i:j])
+                ans = tmp_ans
+                j += 1
+            except ValueError:
+                break
+    return int(ans)
     """
     if answer.endswith('.'):
         answer = answer[:-1]
@@ -17,3 +37,84 @@ def extract_number_from_answer(answer):
         return -1
 
     return number[0]
+    """
+
+
+def split_question(problem):
+    """
+    Separates the last sentence from the problem and treats it as the question
+    """
+    sentences = problem.strip().split('.')
+    split = 1
+    while len('.'.join(sentences[-split:])) < 5 and split < len(sentences):
+        split += 1
+    problem = '.'.join(sentences[:-split])
+    question = '.'.join(sentences[-split:])
+    return problem, question
+
+
+def load_strategy(f):
+    """
+    load strategy's state graph from file
+    Format:
+        STATE_NAME
+        ####
+        PROMPT
+        ######
+        regex1 ### NEXT_STATE1
+        regex2 ### NEXT_STATE2
+        ...
+        ######
+    Strategy needs to have START state and terminate in END state (transitioning to end state terminates conversation).
+    Regexes are checked in order of appearance and are case-insensitive,
+        make sure every possible response is covered by regexes.
+
+    Empty lines and "# ...." (python comment lines) are ignored
+
+    return
+        prompts - state_name -> prompt dictionary
+        states - state_name -> (regex -> state_name dictionary) dictionary
+    """
+    states = {}
+    prompts = {}
+    phase = 0  # 0 - name, 1 - prompt, 2 - transitions
+    prompt = None
+    current_state = None
+    for line in f:
+        line = line.strip()
+        if line == '####':
+            prompt = None
+            phase = 1
+            continue
+        elif line == '######':
+            prompts[current_state] = prompt
+            phase = 2
+            continue
+        elif line == '########':
+            phase = 0
+            continue
+        elif re.fullmatch('#.*', line, re.DOTALL) is not None or len(line) == 0:
+            # skip comment lines and empty lines
+            continue
+        if phase == 0:
+            current_state = line.upper()
+            states[current_state] = {}
+        elif phase == 1:
+            if prompt is None:
+                prompt = line
+            else:
+                prompt += '\n' + line
+        elif phase == 2:
+            line = line.split('###')
+            regex = line[0].strip().lower()
+            next_state = line[1].strip().upper()
+            states[current_state][regex] = next_state
+    print('Interactive strategy state graph:', states)
+    return prompts, states
+
+
+def log_conversation(conversation):
+    out = '\n'
+    for msg in conversation:
+        out = out + f'{msg["role"].strip().upper()}  : {msg["content"].strip()}\n'
+    return out
